8e93c930352fc6e7a57e3302f7e85a8fa8800f74
gayaxh <gayash.pamudu@gmail.com>
2025-04-22 10:29:16 +0530

    Add article management methods to Child controller

--- app/controllers/Child.php
+++ app/controllers/Child.php
@@ -3,6 +3,8 @@ class Child extends Controller {
     private $childModel;
     private $bookModel;
     private $commentModel;
+    private $favoriteModel;
+    private $articleModel;
 
     public function __construct() {
         // Check if logged in and correct role
@@ -15,6 +17,8 @@ class Child extends Controller {
         $this->childModel = $this->model('M_Child');
         $this->bookModel = $this->model('M_Books');
         $this->commentModel = $this->model('M_Comments');
+        $this->favoriteModel = $this->model('M_Favorites');
+        $this->articleModel = $this->model('M_Articles');
     }
     
     /**
@@ -170,6 +174,64 @@ class Child extends Controller {
         redirect('child/viewBook/' . $bookId);
     }
 
+    /**
+     * Edit a comment
+     * @param int $commentId The comment ID to edit
+     * @param int $bookId The book ID (for redirection)
+     * @return void
+     */
+    public function editComment($commentId, $bookId) {
+        // Get the comment to check if it belongs to the user
+        $comment = $this->commentModel->getCommentById($commentId);
+        
+        if(!$comment || $comment->user_id != $_SESSION['user_id']) {
+            flash('comment_error', 'You are not authorized to edit this comment', 'alert alert-danger');
+            redirect('child/viewBook/' . $bookId);
+        }
+        
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // Sanitize POST data
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            
+            // Get form data
+            $data = [
+                'comment_id' => $commentId,
+                'book_id' => $bookId,
+                'user_id' => $_SESSION['user_id'],
+                'comment' => trim($_POST['comment']),
+                'comment_err' => ''
+            ];
+            
+            // Validate comment
+            if(empty($data['comment'])) {
+                $data['comment_err'] = 'Please enter a comment';
+            }
+            
+            // Make sure there are no errors
+            if(empty($data['comment_err'])) {
+                // Update comment
+                if($this->commentModel->updateComment($data['comment_id'], $data['user_id'], $data['comment'])) {
+                    flash('comment_success', 'Your comment has been updated', 'alert alert-success');
+                } else {
+                    flash('comment_error', 'Something went wrong. Please try again.', 'alert alert-danger');
+                }
+                redirect('child/viewBook/' . $bookId);
+            } else {
+                flash('comment_error', $data['comment_err'], 'alert alert-danger');
+                redirect('child/viewBook/' . $bookId);
+            }
+        } else {
+            // Get the current comment text
+            $data = [
+                'comment_id' => $commentId,
+                'book_id' => $bookId,
+                'comment' => $comment->comment
+            ];
+            
+            $this->view('pages/child/v_edit_comment', $data);
+        }
+    }
+
     // View book details
     public function viewBook($bookId = null) {
         if(!$bookId) {
@@ -186,16 +248,302 @@ class Child extends Controller {
         $childId = $_SESSION['user_id'];
         $existingRequest = $this->childModel->getBookRequestStatus($childId, $bookId);
         
+        // Check if book is in favorites
+        $isFavorited = $this->favoriteModel->isBookFavorited($childId, $bookId);
+        
         // Get comments for this book
         $comments = $this->commentModel->getCommentsByBook($bookId);
 
         $data = [
             'book' => $book,
             'request' => $existingRequest,
-            'comments' => $comments
+            'comments' => $comments,
+            'is_favorited' => $isFavorited
         ];
 
         $this->view('pages/child/v_book_detail', $data);
+    }
+
+    /**
+     * Add a book to favorites
+     * @param int $bookId The book ID
+     * @return void
+     */
+    public function addToFavorites($bookId = null) {
+        // If no book ID provided, redirect back
+        if(!$bookId) {
+            flash('favorite_error', 'Invalid book selection', 'alert alert-danger');
+            redirect('pages/index');
+        }
+
+        // Add to favorites
+        $userId = $_SESSION['user_id'];
+        
+        if($this->favoriteModel->addFavorite($userId, $bookId)) {
+            flash('favorite_success', 'Book added to your favorites', 'alert alert-success');
+        } else {
+            flash('favorite_error', 'Unable to add this book to favorites. Please try again.', 'alert alert-danger');
+        }
+        
+        // Redirect back to the book page
+        redirect('child/viewBook/' . $bookId);
+    }
+
+    /**
+     * Remove a book from favorites
+     * @param int $bookId The book ID
+     * @return void
+     */
+    public function removeFromFavorites($bookId = null) {
+        // If no book ID provided, redirect back
+        if(!$bookId) {
+            flash('favorite_error', 'Invalid book selection', 'alert alert-danger');
+            redirect('pages/index');
+        }
+
+        // Remove from favorites
+        $userId = $_SESSION['user_id'];
+        
+        if($this->favoriteModel->removeFavorite($userId, $bookId)) {
+            flash('favorite_success', 'Book removed from your favorites', 'alert alert-success');
+        } else {
+            flash('favorite_error', 'Unable to remove this book from favorites. Please try again.', 'alert alert-danger');
+        }
+        
+        // Redirect back to the book page
+        redirect('child/viewBook/' . $bookId);
+    }
+
+    /**
+     * View favorite books
+     * @return void
+     */
+    public function favorites() {
+        $userId = $_SESSION['user_id'];
+        $favoriteBooks = $this->favoriteModel->getFavoriteBooks($userId);
+        
+        $data = [
+            'books' => $favoriteBooks,
+            'page_title' => 'My Favorite Books'
+        ];
+        
+        $this->view('pages/child/v_favorites', $data);
+    }
+
+    /**
+     * View all articles written by children
+     * @return void
+     */
+    public function articles() {
+        // Get all published articles
+        $articles = $this->articleModel->getAllArticles();
+        
+        $data = [
+            'articles' => $articles,
+            'page_title' => 'Child Articles'
+        ];
+        
+        $this->view('pages/child/v_articles', $data);
+    }
+
+    /**
+     * View my articles (articles written by the current child user)
+     * @return void
+     */
+    public function myArticles() {
+        $userId = $_SESSION['user_id'];
+        $articles = $this->articleModel->getArticlesByUser($userId);
+        
+        $data = [
+            'articles' => $articles,
+            'page_title' => 'My Articles'
+        ];
+        
+        $this->view('pages/child/v_my_articles', $data);
+    }
+
+    /**
+     * View a single article
+     * @param int $articleId The article ID
+     * @return void
+     */
+    public function viewArticle($articleId = null) {
+        if(!$articleId) {
+            redirect('child/articles');
+        }
+
+        $article = $this->articleModel->getArticleById($articleId);
+
+        if(!$article) {
+            redirect('child/articles');
+        }
+
+        $data = [
+            'article' => $article
+        ];
+
+        $this->view('pages/child/v_article_detail', $data);
+    }
+
+    /**
+     * Create a new article
+     * @return void
+     */
+    public function createArticle() {
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // Sanitize POST data
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            
+            // Get form data
+            $data = [
+                'title' => trim($_POST['title']),
+                'content' => trim($_POST['content']),
+                'image_url' => isset($_POST['image_url']) ? trim($_POST['image_url']) : null,
+                'title_err' => '',
+                'content_err' => ''
+            ];
+            
+            // Validate title
+            if(empty($data['title'])) {
+                $data['title_err'] = 'Please enter a title';
+            }
+            
+            // Validate content
+            if(empty($data['content'])) {
+                $data['content_err'] = 'Please enter some content';
+            }
+            
+            // Make sure there are no errors
+            if(empty($data['title_err']) && empty($data['content_err'])) {
+                // Add article
+                if($articleId = $this->articleModel->addArticle($_SESSION['user_id'], $data['title'], $data['content'], $data['image_url'])) {
+                    flash('article_success', 'Your article has been published', 'alert alert-success');
+                    redirect('child/viewArticle/' . $articleId);
+                } else {
+                    flash('article_error', 'Something went wrong. Please try again.', 'alert alert-danger');
+                    $this->view('pages/child/v_create_article', $data);
+                }
+            } else {
+                // Load view with errors
+                $this->view('pages/child/v_create_article', $data);
+            }
+        } else {
+            $data = [
+                'title' => '',
+                'content' => '',
+                'image_url' => '',
+                'title_err' => '',
+                'content_err' => ''
+            ];
+            
+            $this->view('pages/child/v_create_article', $data);
+        }
+    }
+
+    /**
+     * Edit an existing article
+     * @param int $articleId The article ID
+     * @return void
+     */
+    public function editArticle($articleId = null) {
+        if(!$articleId) {
+            redirect('child/myArticles');
+        }
+        
+        // Check if article exists and belongs to user
+        $article = $this->articleModel->getArticleById($articleId);
+        
+        if(!$article || $article->user_id != $_SESSION['user_id']) {
+            flash('article_error', 'You are not authorized to edit this article', 'alert alert-danger');
+            redirect('child/myArticles');
+        }
+        
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // Sanitize POST data
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            
+            // Get form data
+            $data = [
+                'article_id' => $articleId,
+                'title' => trim($_POST['title']),
+                'content' => trim($_POST['content']),
+                'image_url' => isset($_POST['image_url']) ? trim($_POST['image_url']) : null,
+                'title_err' => '',
+                'content_err' => ''
+            ];
+            
+            // Validate title
+            if(empty($data['title'])) {
+                $data['title_err'] = 'Please enter a title';
+            }
+            
+            // Validate content
+            if(empty($data['content'])) {
+                $data['content_err'] = 'Please enter some content';
+            }
+            
+            // Make sure there are no errors
+            if(empty($data['title_err']) && empty($data['content_err'])) {
+                // Update article
+                if($this->articleModel->updateArticle($articleId, $_SESSION['user_id'], $data['title'], $data['content'], $data['image_url'])) {
+                    flash('article_success', 'Your article has been updated', 'alert alert-success');
+                    redirect('child/viewArticle/' . $articleId);
+                } else {
+                    flash('article_error', 'Something went wrong. Please try again.', 'alert alert-danger');
+                    $this->view('pages/child/v_edit_article', $data);
+                }
+            } else {
+                // Load view with errors
+                $this->view('pages/child/v_edit_article', $data);
+            }
+        } else {
+            $data = [
+                'article_id' => $articleId,
+                'title' => $article->title,
+                'content' => $article->content,
+                'image_url' => $article->image_url,
+                'title_err' => '',
+                'content_err' => ''
+            ];
+            
+            $this->view('pages/child/v_edit_article', $data);
+        }
+    }
+
+    /**
+     * Delete an article
+     * @param int $articleId The article ID
+     * @return void
+     */
+    public function deleteArticle($articleId = null) {
+        if(!$articleId) {
+            redirect('child/myArticles');
+        }
+        
+        // Check if article exists and belongs to user
+        $article = $this->articleModel->getArticleById($articleId);
+        
+        if(!$article || $article->user_id != $_SESSION['user_id']) {
+            flash('article_error', 'You are not authorized to delete this article', 'alert alert-danger');
+            redirect('child/myArticles');
+        }
+        
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            if($this->articleModel->deleteArticle($articleId, $_SESSION['user_id'])) {
+                flash('article_success', 'Article deleted successfully', 'alert alert-success');
+            } else {
+                flash('article_error', 'Something went wrong. Please try again.', 'alert alert-danger');
+            }
+            
+            redirect('child/myArticles');
+        } else {
+            // Confirmation page
+            $data = [
+                'article' => $article
+            ];
+            
+            $this->view('pages/child/v_delete_article', $data);
+        }
     }
 }
 
