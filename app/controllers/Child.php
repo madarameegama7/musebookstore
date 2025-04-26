a83074afe37f1c5fffcb58b12ac24fb37ea7ddd1
gayaxh <gayash.pamudu@gmail.com>
2025-04-26 13:22:16 +0530

    Nav bar changes

--- app/controllers/Child.php
+++ app/controllers/Child.php
@@ -41,31 +41,7 @@ class Child extends Controller {
         $this->view('pages/child/v_childhome', $data);
     }
     
-    public function bookRelease(){
-        $data = [];
-        $this->view('pages/child/v_newbookrelease', $data);
-    }
-    
-    public function childAuthourAward(){
-        $data = [];
-        $this->view('pages/child/v_childauthouraward', $data);
-    }
-     
-    public function childAuto(){
-        $data = [];
-        $this->view('pages/child/v_childauto', $data);
-    }
-    
-    public function childTopBooks(){
-        $data = [];
-        $this->view('pages/child/v_childtopbooks', $data);
-    }
-    
-    public function childCreative(){
-        $data = [];
-        $this->view('pages/child/v_childtopbooks', $data);
-    }
-    
+        
     /**
      * Search for books by title, author, or genre
      */
