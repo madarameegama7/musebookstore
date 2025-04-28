e7f63186866a40954f9e75edb43c03300a349f6e
gayaxh <gayash.pamudu@gmail.com>
2025-04-29 02:02:40 +0530

    last

--- app/views/pages/child/v_create_article.php
+++ app/views/pages/child/v_create_article.php
@@ -194,23 +194,5 @@
     </div>
 </div>
 
-<script src="https://cdn.ckeditor.com/ckeditor5/29.0.0/classic/ckeditor.js"></script>
-<script>
-    ClassicEditor
-        .create(document.querySelector('#article-editor'), {
-            toolbar: ['heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', '|', 'indent', 'outdent', '|', 'undo', 'redo'],
-            heading: {
-                options: [
-                    { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
-                    { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
-                    { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
-                    { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' }
-                ]
-            }
-        })
-        .catch(error => {
-            console.error(error);
-        });
-</script>
 
 <?php require APPROOT.'/views/inc/footer.php'; ?>
