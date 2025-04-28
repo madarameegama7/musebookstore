d5a0c9084bd976d0037a2ffb14dbe623e342e179
Kavirubc <hapuarachchikaviru@gmail.com>
2025-04-28 21:45:09 +0530

    Update book management links to include 'book' in the URL path for consistency

--- app/views/pages/admin/v_edit_book.php
+++ app/views/pages/admin/v_edit_book.php
@@ -6,14 +6,14 @@
     <?php require APPROOT . '/views/inc/components/admin/sidebar.php'; ?>
 
     <main class="admin-main-content">
-        <a href="<?php echo URLROOT; ?>/admin/manageBooks" class="btn btn-back"><i class="fa fa-arrow-left"></i> Back to Books</a>
+        <a href="<?php echo URLROOT; ?>/admin/book/manageBooks" class="btn btn-back"><i class="fa fa-arrow-left"></i> Back to Books</a>
 
         <?php flash('admin_msg'); ?> <!-- Display flash messages if update fails on reload -->
 
         <h2><?php echo $data['title']; ?></h2>
         <p>Edit book details below.</p>
 
-        <form class="edit-book-form" action="<?php echo URLROOT; ?>/admin/updateBook/<?php echo $data['bookid']; ?>" method="post">
+        <form class="edit-book-form" action="<?php echo URLROOT; ?>/admin/book/updateBook/<?php echo $data['bookid']; ?>" method="post">
 
             <label for="booktitle">Book Title: <sup>*</sup></label>
             <input type="text" name="booktitle" id="booktitle" value="<?php echo htmlspecialchars($data['booktitle'] ?? ''); ?>" required>
